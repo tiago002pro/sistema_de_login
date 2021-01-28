5645d69d18d3591c29e8183411e3a5e0691de10e
Tiago Barbosa <tiagobarbosa02@outlook.com>
2021-01-28 17:54:55 -0300

    Iniciando o Back-End

---
+++ app/core/Core.php
@@ -0,0 +1,38 @@
+<?php
+
+    class core
+    {
+        private $url;
+
+        private $controller;
+        private $method;
+        private $params = array();
+
+        public function _construction()
+        {
+
+        }
+
+        public function start($request)
+        {
+            if($request['url']){
+                $this->url = explode('/', $request['url']);
+    
+                $this->controller = ucfirst($this->utl[0]).'Controller';
+                array_shift($this->url);
+    
+                $this->method = $this->url[0];
+                array_shift($this->url);
+    
+                $this->params = $this->url;
+            } else {
+                $this->controller = 'LoginController';
+                $this->method = 'index';
+            }
+
+            call_user_func(array(new $this->controller, $this->method, $this->params));
+            var_dump($this->controller, $this->method, $this->params);
+
+        }
+    }
+?>
